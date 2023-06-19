d417848f1153d5814eeb2e020ee0d2ca8d24121b
David Armstrong <d.b.armstrong@sasktel.net>
2023-06-18 20:47:59 -0600

    LED matrix set, switcch matrix in process

--- front_panel/scan_matrix.v
+++ front_panel/scan_matrix.v
@@ -1,4 +1,4 @@
-
+/* verilator lint_off ASCRANGE */
 
 module scan_matrix (
     input clk,
@@ -7,7 +7,7 @@ module scan_matrix (
     input [0:2] EMA,
     input [0:11] ds,
     output [0:2] LR,
-    output [0:10] LC,
+    output reg [0:11] LC,
     output [0:5] SR,
     output [0:11] sr,
 	output [2:0] dsel,
@@ -39,28 +39,42 @@ module scan_matrix (
       case (Lrow)
         0: begin
           Lrow <= 2'd1;
-          LC   <= {EMA[2:0], A[0:6], dsel1[0:1]};
+          LC   <= {EMA[0:2], A[0:6], dsel1[0:1]};
         end
         1: begin
           LC   <= {A[7:11], ds[0:4], dsel1[2:3]};
           Lrow <= 2'd2;
         end
         2: begin
-          LC   <= {ds[5:11], single_step, halt, dsel1[4:5]};
+          LC   <= {ds[5:11], single_step, halt,sw, dsel1[4:5]};
           Lrow <= 2'd0;
         end
         default: Lrow <= 2'b00;
       endcase
       case (Srow)
         0: begin
+	   SR <= 6'b100000;
+	   Srow <= 3'b001;
         end
         1: begin
+	   SR <= 6'b010000;
+	   Srow <= 3'b010;
         end
         2: begin
+	   SR <= 6'b001000;
+	   Srow <= 3'b011;
         end
         3: begin
+	   SR <= 6'b000100;
+	   Srow <= 3'b100;
         end
         4: begin
+	   SR <= 6'b000010;
+	   Srow <= 3'b101;
+        end
+        5: begin
+	   SR <= 6'b000001;
+	   Srow <= 3'b000;
         end
         default: Srow <= 3'b000;
       endcase
