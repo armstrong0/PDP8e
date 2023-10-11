7d27e7158ca964ee111bde38d480fec1043c565a
David Armstrong <d.b.armstrong@sasktel.net>
2023-10-11 14:58:42 -0600

    moved serial test to IO test

--- FPGA_IO_test/PDP8e.v
+++ FPGA_IO_test/PDP8e.v
@@ -1,31 +1,44 @@
 // top level for a PDP8e
 
-parameter real clock_frequency = 66000000;
-
+`ifndef SIM
 `include "pll.v"
-`include "../front_panel/front_panel.v"
-`include "../state_machine/state_machine.v"
-`include "../ma/ma.v"
+`endif
+`include "serial_tx.v"
+`include "serial_rx.v"
 `default_nettype none
 
 
+/* verilator lint_off LITENDIAN */
 
-module top (input clk,
-	    output led1,output led2,
-	    output runn,
-	    output [0:2] EMAn, 
-        output [0:11] An,
-	    output [0:11] dsn,
-	    input [0:11] sr,
-	    input [0:5] dsel,
-	    input dep, input sw,
-	    input single_step, input halt, input examn, input contn,
-	    input extd_addrn, input addr_loadn, input clearn );
+module PDP8e (input clk12,
+    output reg led1,output reg led2,
+    output runn,
+    output [0:2] EMAn,
+    output [0:11] An,
+    output [0:11] dsn,
+    output tx,
+`ifdef SIM
+    input clk100,
+    input pll_locked,
+    input reset,
+`endif
+    input rx,
+    input [0:11] sr,
+    input [0:5] dsel,
+    input dep, input sw,
+    input single_step, input halt, input examn, input contn,
+    input extd_addrn, input addr_loadn, input clearn
+);
     /* I/O */
+    wire [0:2] EMA;
     assign EMAn = ~EMA;
+    wire [0:11] A;
     assign An = ~A;
-    assign dsn = ~ds;     
-    wire run; 
+    //wire [0:11] ds;
+    reg [0:11] ds;
+    assign dsn = ~ds;
+    wire run;
+
     assign runn = ~run;
     wire exam;
     assign exam = ~examn;
@@ -38,167 +51,130 @@ module top (input clk,
     wire clear;
     assign clear = ~clearn;
 
-	wire [2:0] EMA;
-	wire [0:11] A;
-	wire [0:11] ds;
-    
-    reg [0:11] pc;
-    wire [0:11] ma;
-    reg [0:11] ac;
-    reg [0:11] mq;
-    reg [0:11] ir;
-    reg L;
-    reg [0:6] savereg;
-    reg [4:0]  state;
-    wire carry;
-    wire L_out;
-    wire [0:11] bselo;
-    wire [0:11] aselo;
-    wire [0:1]  aselc;
-    wire [0:11] shift_aco;
-    
-`include "../parameters.v"
-/* verilator lint_off LITENDIAN */
+    wire mskip,skip,eskip;
+    wire [0:11] ac;
+    wire [0:11] serial_data_bus,instruction;
+    wire sskip;
+    wire [4:0]  state;
+    reg [0:11] rsr;
+    reg UF;
 
-   
-    wire clk100; 
-    wire pll_locked;
-    reg [0:11] instruction;
-    wire int_in_prog,addr_loadd,depd,examd,isz_skip,irq,int_inh,int_ena,contd;
-    wire triggerd,cleard,extd_addrd;
-	
-	wire sw_active;
-	wire index;
-    //assign int_in_prog = 0;
-    reg [0:11] rac,rsr;
-    reg [31:0] counter;
-	reg [0:2] DF,IF;
-	reg UF;
+    wire [11:0] tx_char;
+	wire [11:0] rx_char;
 
-	reg EAE_loop,EAE_mode,gtf;
-	wire EAE_skip;
-	
-  // assign int_in_prog = 0;
-   assign int_ena = 0;
-   
-    pll p1(.clock_in (clk),
-        .clock_out (clk100),
-	.locked (pll_locked ));
+`include "HX_clock.v"
 
     reg [3:0] pll_locked_buf;   // reset circuit by Cliff Wolf
+`ifndef SIM
+    reg reset;
+    wire clk100;
+    wire pll_locked;
+    pll p1(.clock_in (clk12),
+        .clock_out (clk100),
+        .locked (pll_locked));
 
-    wire reset;
-    always @(posedge clk)
-        pll_locked_buf <= {pll_locked_buf[2:0],pll_locked};
-    assign reset = ~pll_locked_buf[3];	
-
-    always @(posedge clk100)
+    always @(posedge clk12)
     begin
-       rsr <= ~sr;
-	   if (reset == 1'b1)
-	   begin
-          rac = 12'o0000;
-		  DF <= 3'b0;
-		  IF <= 3'b0;
-		  UF <= 1'b0;
-		  EAE_loop <= 1'b0;
-		  EAE_mode <= 1'b0;
-		  gtf <= 1'b0;
-		  pc <= 12'd0;
-	   end	  
+        pll_locked_buf <= {pll_locked_buf[2:0],pll_locked};
+        reset <= ~pll_locked_buf[3];
+        rsr <= sr;
     end
+`else
+    always @(posedge clk12)
+    begin
+        rsr <= sr;
+    end
+`endif
+    reg [24:0] counter2;
 
+always @(posedge clk100)
+begin
+if (reset == 1) counter2 <= 'o0;
+else
+    counter2 <= counter2 +1;
+end
 
-    ma ma1(.clk (clk100),
-        .reset (reset),
-        .state (state),
-        .instruction (instruction),
-        .pc (pc),
-        .ma (ma),
-        .mq (mq),
-        .ac (rac),
-        .sr (rsr),
-    	//.addr (A),
-        .int_in_prog (int_in_prog),
-        .addr_loadd (addr_loadd),
-        .depd (depd),
-        .examd (examd),
-		.sw (sw),
-		.IF (IF),
-		.DF (DF),
-		.EMA (EMA),
-       // .mdout (ds),  //mdout
-		.index (index),
-        .isz_skip (isz_skip));
+    assign {EMA,A} = counter2[24:10];
+    assign run = ~rx;
 
-   state_machine sm1(.clk (clk100),
-        .reset (reset),
-        .state (state),
-        .instruction (instruction),
-
-		.ac (ac),   // bad design?  needed for a singel instruction DPSZ
-		.mq (mq),
-        
-		.index (index),  
-
-        .int_req (irq),
-        .int_inh (int_inh),
-        .int_ena (int_ena),
-        .int_in_prog (int_in_prog),
-		.UF (UF),
-
-		.EAE_loop (EAE_loop),
-		.EAE_mode (EAE_mode),
-		.EAE_skip (EAE_skip),
-		.gtf (gtf),
-		
-        .halt (halt),
-        .single_step (single_step),
-        .cont (contd),
-        .trigger (triggerd));
-
-   front_panel FP(.clk (clk100),
-        .reset (reset),
-        .state (state),
-        .clear (clear),
-        .extd_addr (extd_addr),
-        .addr_load (addr_load),
-        .dep (dep),
-        .exam (exam),
-        .cont (cont),
-        .sing_step (single_step),
-        .halt (halt),
-
-        .triggerd (triggerd),
-        .cleard (cleard),
-        .extd_addrd (extd_addrd),
-        .addr_loadd (addr_loadd),
-        .depd (depd),
-        .examd (examd),
-        .contd (contd),
-		
-		.sw_active (sw_active)
-	);
-
-
-
-
-
-    assign {led1,led2 } = counter[31:30] ^ 2'b11; 
-    // assign run = 1;  // test light polarity - LED's are inverted... light
-    // is off
-     assign run = sw;  // down is light on up is off so up is 1
-
+//`include "../parameters.v"
+// this section established the baud rate and transmits charactors at that
+// baud rate
+// It transmits in order ascii charactors starting at space and ending at ~.
+// so it includes numbers punctuation upper and lower case alphabet.
+// The output is arranged in 80 column lines.
+// lines are terminated with cr / lf.
 
     
-    assign A[0:11] = ma[0:11];
-	/* verilator lint_off SELRANGE */
-    assign EMA[0:2] = savereg[4:6];
-    assign ds[0:4] = state; 
-	assign ds[5:11] = counter[31:25];
-    /* Count up on every edge of the incoming 12MHz clk */
-    always @ (posedge clk100) begin
-        counter <= counter + 1;
+serial_tx TX(
+	.clk100 (clk100),
+	.reset (reset),
+	.tx (tx),
+	.tx_char_out (tx_char));
+
+
+serial_rx RX(
+	.reset (reset),
+	.clk100 (clk100),
+	.rx (rx),
+	.rx_char_out (rx_char));
+
+
+
+// this section selects what is displayed on the 12 bit data display
+//
+    reg [0:11] shft_reg;
+    
+	always @(posedge clk100)
+    begin
+        if (dsel[0] == 1) begin
+            ds <= {4'd0,tx_char}; // displays last char send, very fast
+        end
+        else if (dsel[1] == 1) begin // allows testing of each switch in the switch register
+            ds <= sr;
+        end
+        else if (dsel[2] == 1) begin // allows testing of every other switch
+            ds <= {sw,addr_load,extd_addr,clear,cont,exam,halt,single_step,dep,3'b0};
+        end
+        else if (dsel[3] == 1) begin // this and the next shifts a single bit thru data display
+            ds <= shft_reg;
+        end
+        else if (dsel[4] == 1) begin
+            ds <= shft_reg;
+        end
+        else if (dsel[5] == 1) begin // display rx charactor
+		    ds <= rx_char;
+        end
+    end
+
+// this section implements left and right shift of a single lit LED	
+    reg pps;
+    reg [23:0] pps_cntr;
+    always @(posedge pps) begin
+	if (reset == 1) shft_reg <= 'o0;
+
+	else if (dsel[4] == 1) begin
+            if (shft_reg == 0) shft_reg <= 12'o4000;
+            else shft_reg <= shft_reg >> 1;
+			end
+	  else if (dsel[3] == 1) begin
+            if (shft_reg == 0) shft_reg <= 1;
+            else shft_reg <= shft_reg << 1;
+		end	
+end
+
+// this section generates a 1 pps clock
+
+    always @(posedge clk12)
+    begin
+        if (pps_cntr == 0)
+        begin
+            pps <= ~ pps;
+            pps_cntr <= 'd6000000;
+        end
+        else
+            pps_cntr <= pps_cntr -1;
+
     end
 
 endmodule
