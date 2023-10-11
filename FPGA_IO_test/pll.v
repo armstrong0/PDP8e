4c4dab38b5501c247e0a817be9e5e23386c4da26
David Armstrong <d.b.armstrong@sasktel.net>
2023-10-11 15:03:10 -0600

    serial test files moved to IO test, better more current testing

--- FPGA_IO_test/pll.v
+++ FPGA_IO_test/pll.v
@@ -6,8 +6,8 @@
  * Use at your own risk.
  *
  * Given input frequency:        12.000 MHz
- * Requested output frequency:   66.000 MHz
- * Achieved output frequency:    66.000 MHz
+ * Requested output frequency:   74.000 MHz
+ * Achieved output frequency:    73.500 MHz
  */
 
 module pll(
@@ -19,8 +19,8 @@ module pll(
 SB_PLL40_CORE #(
 		.FEEDBACK_PATH("SIMPLE"),
 		.DIVR(4'b0000),		// DIVR =  0
-		.DIVF(7'b1010111),	// DIVF = 87
-		.DIVQ(3'b100),		// DIVQ =  4
+		.DIVF(7'b0110000),	// DIVF = 48
+		.DIVQ(3'b011),		// DIVQ =  3
 		.FILTER_RANGE(3'b001)	// FILTER_RANGE = 1
 	) uut (
 		.LOCK(locked),
