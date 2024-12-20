bebc74d659488f4c87a8339890b5a9f1d6f8aaec
David Armstrong <d.b.armstrong@sasktel.net>
2024-12-20 12:32:11 -0600

    files for a make which exhibits an extnpr problem I Think

--- FPGA_IO_test/PDP8e.v
+++ FPGA_IO_test/PDP8e.v
@@ -10,11 +10,12 @@
 
 /* verilator lint_off LITENDIAN */
 
-module PDP8e (input clk12,
-    output reg led1,output reg led2,
+module PDP8e (
+    input clk,
+    output reg led1,
+    output reg led2,
     output runn,
-    output [0:2] EMAn,
-    output [0:11] An,
+    output [0:14] An,
     output [0:11] dsn,
     output tx,
 `ifdef SIM
@@ -24,157 +25,195 @@ module PDP8e (input clk12,
 `endif
     input rx,
     input [0:11] sr,
-    input [0:5] dsel,
-    input dep, input sw,
-    input single_step, input halt, input examn, input contn,
-    input extd_addrn, input addr_loadn, input clearn
+    input dsel_swn,
+    output reg [4:0] dsel_led,
+    input dep,
+    input sw,
+    input single_stepn,
+    input haltn,
+    input examn,
+    input contn,
+    input extd_addrn,
+    input addr_loadn,
+    input clearn
 );
-    /* I/O */
-    wire [0:2] EMA;
-    assign EMAn = ~EMA;
-    wire [0:11] A;
-    assign An = ~A;
-    //wire [0:11] ds;
-    reg [0:11] ds;
-    assign dsn = ~ds;
-    wire run;
+  /* I/O */
+  assign An = ~addr;
+  wire [0:14] addr;
+  //wire [0:11] ds;
+  reg  [0:11] ds;
+  assign dsn = ~ds;
+  wire run;
 
-    assign runn = ~run;
-    wire exam;
-    assign exam = ~examn;
-    wire cont;
-    assign cont = ~contn;
-    wire extd_addr;
-    assign extd_addr = ~extd_addrn;
-    wire addr_load;
-    assign addr_load = ~addr_loadn;
-    wire clear;
-    assign clear = ~clearn;
+  assign runn = ~run;
+  wire exam;
+  assign exam = ~examn;
+  wire cont;
+  assign cont = ~contn;
+  wire extd_addr;
+  assign extd_addr = ~extd_addrn;
+  wire addr_load;
+  assign addr_load = ~addr_loadn;
+  wire clear;
+  assign clear = ~clearn;
+  wire halt;
+  assign halt = ~haltn;
+  wire single_step;
+  assign single_step = ~single_stepn;
 
-    wire mskip,skip,eskip;
-    wire [0:11] ac;
-    wire [0:11] serial_data_bus,instruction;
-    wire sskip;
-    wire [4:0]  state;
-    reg [0:11] rsr;
-    reg UF;
+  wire dsel_sw;
+  assign dsel_sw = ~dsel_swn;
+  reg dsel_true;
+  reg [15:0] dsel_cntr;
 
-    wire [11:0] tx_char;
-	wire [11:0] rx_char;
+  reg [0:11] rsr;
+  reg [5:0] dsel;
 
-`include "HX_clock.v"
+  wire [11:0] tx_char;
+  wire [11:0] rx_char;
 
-    reg [3:0] pll_locked_buf;   // reset circuit by Cliff Wolf
+  `include "HX_clock.v"
+
+  reg [3:0] pll_locked_buf;  // reset circuit by Cliff Wolf
 `ifndef SIM
-    reg reset;
-    wire clk100;
-    wire pll_locked;
-    pll p1(.clock_in (clk12),
-        .clock_out (clk100),
-        .locked (pll_locked));
+  reg  reset;
+  wire clk100;
+  wire pll_locked;
+  pll p1 (
+      .clock_in(clk),
+      .clock_out(clk100),
+      .locked(pll_locked)
+  );
 
-    always @(posedge clk12)
-    begin
-        pll_locked_buf <= {pll_locked_buf[2:0],pll_locked};
-        reset <= ~pll_locked_buf[3];
-        rsr <= sr;
-    end
+  always @(posedge clk) begin
+    pll_locked_buf <= {pll_locked_buf[2:0], pll_locked};
+    reset <= ~pll_locked_buf[3];
+    rsr <= sr;
+  end
 `else
-    always @(posedge clk12)
-    begin
-        rsr <= sr;
-    end
+  always @(posedge clk) begin
+    rsr <= sr;
+  end
 `endif
-    reg [24:0] counter2;
+  reg [24:0] counter2;
 
-always @(posedge clk100)
-begin
-if (reset == 1) counter2 <= 'o0;
-else
-    counter2 <= counter2 +1;
-end
+  always @(posedge clk100) begin
+    if (reset == 1) counter2 <= 'o0;
+    else counter2 <= counter2 + 1;
+  end
 
-    assign {EMA,A} = counter2[24:10];
-    assign run = ~rx;
+  assign addr = counter2[24:10];
+  assign run  = ~rx;
 
-//`include "../parameters.v"
-// this section established the baud rate and transmits charactors at that
-// baud rate
-// It transmits in order ascii charactors starting at space and ending at ~.
-// so it includes numbers punctuation upper and lower case alphabet.
-// The output is arranged in 80 column lines.
-// lines are terminated with cr / lf.
-
-    
-serial_tx TX(
-	.clk100 (clk100),
-	.reset (reset),
-	.tx (tx),
-	.tx_char_out (tx_char));
+  // this section established the baud rate and transmits charactors at that
+  // baud rate
+  // It transmits in order ascii charactors starting at space and ending at ~.
+  // so it includes numbers punctuation upper and lower case alphabet.
+  // The output is arranged in 80 column lines.
+  // lines are terminated with cr / lf.
 
 
-serial_rx RX(
-	.reset (reset),
-	.clk100 (clk100),
-	.rx (rx),
-	.rx_char_out (rx_char));
+  serial_tx TX (
+      .clk100(clk100),
+      .reset(reset),
+      .tx(tx),
+      .tx_char_out(tx_char)
+  );
+
+
+  serial_rx RX (
+      .reset(reset),
+      .clk100(clk100),
+      .rx(rx),
+      .rx_char_out(rx_char)
+  );
 
 
 
-// this section selects what is displayed on the 12 bit data display
-//
-    reg [0:11] shft_reg;
-    
-	always @(posedge clk100)
-    begin
-        if (dsel[0] == 1) begin
-            ds <= {4'd0,tx_char}; // displays last char send, very fast
-        end
-        else if (dsel[1] == 1) begin // allows testing of each switch in the switch register
-            ds <= sr;
-        end
-        else if (dsel[2] == 1) begin // allows testing of every other switch
-            ds <= {sw,addr_load,extd_addr,clear,cont,exam,halt,single_step,dep,3'b0};
-        end
-        else if (dsel[3] == 1) begin // this and the next shifts a single bit thru data display
-            ds <= shft_reg;
-        end
-        else if (dsel[4] == 1) begin
-            ds <= shft_reg;
-        end
-        else if (dsel[5] == 1) begin // display rx charactor
-		    ds <= rx_char;
-        end
+`ifdef SIM
+`define  PPS_CNT 24'd65
+`define  DSEL_CNT 16'd65 
+`else
+`define PPS_CNT 24'd65535
+`define DSEL_CNT  16'd65535
+`endif
+
+  //
+  reg [0:11] shft_reg;
+
+  always @(posedge clk) begin
+    if (reset == 1) begin
+      ds <= 12'd0;
+    end else if (dsel[0] == 1) begin
+      ds <= {4'd0, tx_char};  // displays last char send, very fast
+    end else if (dsel[1] == 1) begin  // allows testing of each switch in the switch register
+      ds <= sr;
+    end else if (dsel[2] == 1) begin  // allows testing of every other switch
+      ds <= {sw, addr_load, extd_addr, clear, cont, exam, halt, single_step, dep, 2'b0, pll_locked};
+    end else if (dsel[3] == 1) begin  // this and the next shifts a single bit thru data display
+      ds <= shft_reg;
+    end else if (dsel[4] == 1) begin
+      ds <= shft_reg;
+    end else if (dsel[5] == 1) begin  // display rx charactor
+      ds <= rx_char;
+    end
+  end
+
+  // this section implements left and right shift of a single lit LED	
+  reg pps;
+  reg [23:0] pps_cntr;
+  always @(posedge clk) begin
+    if (reset == 1) shft_reg <= 'o0;
+
+    else if ((dsel[4] == 1) && (pps == 1)) begin
+      if (shft_reg == 0) shft_reg <= 12'o4000;
+      else shft_reg <= shft_reg >> 1;
+    end else if ((dsel[3] == 1) && (pps == 1)) begin
+      if (shft_reg == 0) shft_reg <= 1;
+      else shft_reg <= shft_reg << 1;
+    end
+  end
+
+  always @(posedge clk) begin
+    if (reset == 1) begin
+      dsel <= 6'b100000;
+      dsel_cntr <= 0;
+      dsel_true <= 0;
+    end else if ((dsel_true == 1) && (dsel_cntr == 0)) begin
+      dsel_cntr <= `DSEL_CNT;
+      dsel <= {dsel[0], dsel[5:1]};
+      dsel_true <= 0;
+    end else if (dsel_cntr != 0) begin
+      dsel_cntr <= dsel_cntr - 1;
+      dsel_true <= 0;
     end
 
-// this section implements left and right shift of a single lit LED	
-    reg pps;
-    reg [23:0] pps_cntr;
-    always @(posedge pps) begin
-	if (reset == 1) shft_reg <= 'o0;
-
-	else if (dsel[4] == 1) begin
-            if (shft_reg == 0) shft_reg <= 12'o4000;
-            else shft_reg <= shft_reg >> 1;
-			end
-	  else if (dsel[3] == 1) begin
-            if (shft_reg == 0) shft_reg <= 1;
-            else shft_reg <= shft_reg << 1;
-		end	
-end
-
-// this section generates a 1 pps clock
-
-    always @(posedge clk12)
-    begin
-        if (pps_cntr == 0)
-        begin
-            pps <= ~ pps;
-            pps_cntr <= 'd6000000;
-        end
-        else
-            pps_cntr <= pps_cntr -1;
-
+    if ((dsel_sw == 1) && (dsel_cntr == 0)) begin
+      dsel_true <= 1;
     end
 
+    case (dsel)
+      6'b100000: dsel_led <= 5'b01100;
+      6'b010000: dsel_led <= 5'b01010;
+      6'b001000: dsel_led <= 5'b01001;
+      6'b000100: dsel_led <= 5'b10100;
+      6'b000010: dsel_led <= 5'b10010;
+      6'b000001: dsel_led <= 5'b10001;
+      default:   dsel_led <= 5'b01100;
+    endcase
+  end
+
+  // this section generates a 1 pps clock
+  always @(posedge clk) begin
+    if (reset == 1) begin
+      pps <= 0;
+      pps_cntr <= `PPS_CNT;
+    end else if (pps_cntr == 0) begin
+      pps <= 'b1;
+      pps_cntr <= `PPS_CNT;
+    end else begin
+      pps_cntr <= pps_cntr - 1;
+      pps <= 0;
+    end
+  end
 endmodule
--- FPGA_IO_test/PDP8e_tb.v
+++ FPGA_IO_test/PDP8e_tb.v
@@ -1,4 +1,4 @@
-`timescale 1 ns / 10 ps
+`timescale 10 ns / 100 ps
 `define pulse(arg) #1 ``arg <=1 ; #140 ``arg <= 0
 
 `define pulse1(arg) #1 ``arg <=1 ; #1400 ``arg <= 0
@@ -11,7 +11,8 @@ module PDP8e_tb;
     reg reset;
     reg  rx;
     reg  [0:11] sr;
-    reg  [0:5] dsel;
+    reg  dsel_sw;
+    wire [0:4] dsel_led;
     reg  dep;
     reg  sw;
     reg  single_step;
@@ -21,16 +22,16 @@ module PDP8e_tb;
     reg  extd_addr;
     reg  addr_load;
     reg  clear ;
-    reg [0:14] address;
+    wire [0:14] An,address;
     wire led1;
     wire led2;
+    wire run;
     wire runn;
-    wire [0:2] EMAn;
-    wire [0:11] An;
     wire [0:11] dsn;
     wire tx;
     wire tclk;
-	reg serial_io;
+    assign address = ~An;
+    assign run = ~runn;
 
 `include "HX_clock.v"
  parameter real baud_rate=9600;
@@ -47,22 +48,13 @@ module PDP8e_tb;
         #42 clk <= 1;
         #42 clk <= 0;
     end
-    always @* begin
-        address <= {~EMAn,~An};
-    end
-    always @(posedge clk100)
-    begin
-        //rx <= tx;  // used in serial tests
-        serial_io <= ((UUT.instruction[0:8] == 9'o603) || 
-	                 (UUT.instruction[0:8] == 9'o604)) ;
-    end
-
+    
+   
 
     PDP8e UUT (.clk12 (clk),
         .runn (runn),
         .led1 (led1),
         .led2 (led2),
-        .EMAn (EMAn),
         .An (An),
         .dsn (dsn),
         .tx (tx),
@@ -71,16 +63,16 @@ module PDP8e_tb;
         .reset (reset),
         .rx (rx),
         .sr (sr),
-        .dsel (dsel),
         .dep (dep),
         .sw (sw),
-        .single_step (single_step),
-        .halt (halt),
+        .single_stepn (~single_step),
+        .haltn (~halt),
         .examn (~exam),
         .contn (~cont),
         .extd_addrn (~extd_addr),
         .addr_loadn (~addr_load),
-        .clearn (~clear)) ;
+        .clearn (~clear),
+        .dsel_swn (~dsel_sw)) ;
 
 
 
@@ -92,7 +84,7 @@ module PDP8e_tb;
 
 
         #1 sr <= 12'o0200;  // normal start address
-         $dumpfile("Hardware_serial_test.vcd");
+         $dumpfile("IO_sim.vcd");
          $dumpvars(0,UUT);
         //        $readmemh( "Diagnostics/dhkaf-a.hex", UUT.MA.ram.mem,0,4095);
         #1 halt <= 1;
@@ -107,13 +99,15 @@ module PDP8e_tb;
         #1 extd_addr <= 0;
         #1 addr_load <= 0;
         #1 dep <= 0;
-        #1 dsel <= 6'b001000;
+        #1 dsel_sw <= 0;
         #1 sr <= 12'o0200;
         #1 pll_locked <= 0;
         #1 rx <= 1; // marking state
         #100 pll_locked <= 1;
         #100 halt <= 0;
-		#150000 rx <= 0;
+		#10 dsel_sw <= 1; 
+        #1000 dsel_sw <= 0;
+        #150000 rx <= 0;
 		#(baud_period*2) rx <=1;
 		#(baud_period) rx <=0;
 		#(baud_period) rx <=1;
@@ -121,8 +115,13 @@ module PDP8e_tb;
 		#(baud_period) rx <=1;
 		#(baud_period) rx <=0;
 		#(baud_period) rx <=1;
+       
+		#1000000 dsel_sw <= 1; 
+        #1000 dsel_sw <= 0;
+        #2000000 dsel_sw <= 1;
 
-        #4000000 $finish;
+        #1000 dsel_sw <= 0;
+        #6000000 $finish;
 
 
     end
