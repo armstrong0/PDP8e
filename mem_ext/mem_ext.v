b53ce62ad8f23f01eddcfb6fd85de56988e860ec
David Armstrong <d.b.armstrong@sasktel.net>
2025-01-12 14:33:28 -0600

    Fixed test bench so it actually runs, move if (UF== 0) outside of casez statements

--- mem_ext/mem_ext.v
+++ mem_ext/mem_ext.v
@@ -24,7 +24,6 @@ module mem_ext (
     output reg [0:11] me_bus
 );
 
-  reg [0:5] SF;
   reg [0:2] IB;
   reg UB;  // user buffer - hold the user flag until JMS or JMP then transferred to UF
   reg [0:6] savereg;
@@ -38,7 +37,6 @@ module mem_ext (
       IF <= 3'o0;
       IB <= 3'o0;
       DF <= 3'o0;
-      SF <= 6'o0;
       UF <= 0;
       UB <= 0;
       UI <= 0;
@@ -48,7 +46,7 @@ module mem_ext (
 
     end else
       case (state)
-        F0: ;
+        F0, FW: ;
         F1:
         case (instruction)  // these should only get executed when
           // in executive mode or in an interrupt
@@ -60,94 +58,91 @@ module mem_ext (
         endcase
 
         F2: begin
-          casez (instruction)
-            // these are prevented from loading into ac by ac
-            // module
-            12'o6004:
-            if (UF == 1'b0) me_bus <= {2'b00, irq, 1'b0, (int_ena | int_delay), savereg};  //GTF
-            12'o6204: if (UF == 1'b0) UI <= 1'b0;  // CUI
-            12'o6214: if (UF == 1'b0) me_bus <= ac | {6'o00, DF, 3'o0};  // RDF
-            12'o6224: if (UF == 1'b0) me_bus <= ac | {6'o00, IF, 3'o0};  // RIF
-            12'o6234: if (UF == 1'b0) me_bus <= ac | {5'o00, savereg};  // RIB
-            12'b1111?????010,  // HLT
-            12'b1111?????100,  // OSR LAS
-            12'b1111?????110:
-            if (UF == 1'b1) UI <= 1'b1;  // OSR LAS
-            12'b1010????????:  // JMP DIRECT
-                        begin
-              if (int_inh == 1) begin
-                ;
-                IF <= IB;
-                UF <= UB;
-                int_inh <= 0;
+          if (UF == 1'b0) begin
+            me_bus <= 12'o0;
+            casez (instruction)
+              // these are prevented from loading into ac by ac
+              // module
+              12'o6004:  //GTF 
+              me_bus <= {2'b00, irq, 1'b0, (int_ena | int_delay), savereg};
+              12'o6204: UI <= 1'b0;  // CUI
+              12'o6214: me_bus <= ac | {6'o00, DF, 3'o0};  // RDF
+              12'o6224: me_bus <= ac | {6'o00, IF, 3'o0};  // RIF
+              12'o6234: me_bus <= ac | {5'o00, savereg};   // RIB
+              12'b1010????????: begin  // JMP DIRECT
+                if (int_inh == 1) begin
+                  IF <= IB;
+                  UF <= UB;
+                  int_inh <= 0;
+                end
               end
-            end
-            default: ;
-          endcase
-          // don't put this in the above case
-          if ((instruction[0:2] == 3'b110) && (UF == 1'b1)) UI <= 1;
+              default: ;
+            endcase
+          end else begin  //in user mode handling traps
+            casez (instruction)
+              12'o6???,  // IOT
+              12'b1111?????010,  // HLT
+              12'b1111?????100,  // OSR LAS
+              12'b1111?????110:
+              UI <= 1'b1;
+              default: ;
+            endcase
+          end
           if (int_delay == 1) begin
             int_ena   <= 1;
             int_delay <= 0;
           end
         end
         F3: begin
-          casez (instruction)
-            12'o6000:
-            if (UF == 1'b0) begin  //SKON
-              int_delay <= 0;
-              int_ena   <= 0;
-            end
-            12'o6001: if (UF == 1'b0) int_delay <= 1;  //ION
-            12'o6002:
-            if (UF == 1'b0) begin  //IOF
-              int_delay <= 0;
-              int_ena   <= 0;
-            end
-            12'o6005:
-            if (UF == 1'b0) //RTF
-                        begin
-              {UB, IB, DF} <= ac[5:11];
-              int_delay <= 1;
-              int_inh <= 1;
-            end
-            12'o6007:
-            if (UF == 1'b0) begin  //CAF clear all flags
-              int_ena <= 0;
-              int_delay <= 0;
-              UF <= 0;
-              // UB <= 0;
-              UI <= 0;
-            end else UI <= 1'b1;
-            12'o62?1: if (UF == 1'b0) DF <= instruction[6:8];  //62N1 CDF
-            12'o62?2:
-            if (UF == 1'b0) begin
-              IB <= instruction[6:8];  //62N2 CIF
-              int_inh <= 1;
-            end
-            12'o62?3:
-            if (UF == 1'b0) begin
-              IB <= instruction[6:8];  //62N3 CDF,CIF
-              DF <= instruction[6:8];
-              int_inh <= 1;
-            end
-            12'o6204: if (UF == 1'b0) UI <= 0;
-            12'o6244:
-            if (UF == 1'b0) begin  //RMF
-              UB <= savereg[0];
-              IB <= savereg[1:3];
-              DF <= savereg[4:6];
-              int_inh <= 1;
-            end
-            12'o6254: ;  // SINT
-            12'o6264: if (UF == 1'b0) UB <= 0;  // CUF
-            12'o6274:
-            if (UF == 1'b0) begin
-              UB <= 1;
-              int_inh <= 1;
-            end
-            default:  ;
-          endcase
+          if (UF == 1'b0) begin
+            casez (instruction)
+              12'o6000: begin  //SKON
+                int_delay <= 0;
+                int_ena   <= 0;
+              end
+              12'o6001: int_delay <= 1;  //ION
+              12'o6002: begin  //IOF
+                int_delay <= 0;
+                int_ena   <= 0;
+              end
+              12'o6005: begin  //RTF
+                {UB, IB, DF} <= ac[5:11];
+                int_delay <= 1;
+                int_inh <= 1;
+              end
+              12'o6007: begin  //CAF clear all flags
+                int_ena <= 0;
+                int_delay <= 0;
+                UF <= 0;
+                // UB <= 0;
+                UI <= 0;
+              end
+              12'o62?1: DF <= instruction[6:8];  //62N1 CDF
+              12'o62?2: begin
+                IB <= instruction[6:8];  //62N2 CIF
+                int_inh <= 1;
+              end
+              12'o62?3: begin
+                IB <= instruction[6:8];  //62N3 CDF,CIF
+                DF <= instruction[6:8];
+                int_inh <= 1;
+              end
+              12'o6204: UI <= 0;
+              12'o6244: begin     //RMF
+                UB <= savereg[0];
+                IB <= savereg[1:3];
+                DF <= savereg[4:6];
+                int_inh <= 1;
+              end
+              12'o6254: ;         // SINT
+              12'o6264: UB <= 0;  // CUF
+              12'o6274: begin     // SUF
+                UB <= 1;
+                int_inh <= 1;
+              end
+              default:  ;
+            endcase
+          end
           mskip <= 0;
         end
         D0: ;
@@ -158,18 +153,16 @@ module mem_ext (
           UF <= UB;
         end
         D1, D2, D3: ;
-        E0:begin
-        if (int_in_prog == 1'b1) begin
-          savereg <= {UF, IF, DF};
-          IF <= 3'o0;
-          DF <= 3'o0;
-          UF <= 0;
-          UB <= 0;
-          int_ena <= 0;
-          int_delay <= 0;
-        end
-        else
-          if (instruction[0:2] == JMS) begin
+        E0: begin
+          if (int_in_prog == 1'b1) begin
+            savereg <= {UF, IF, DF};
+            IF <= 3'o0;
+            DF <= 3'o0;
+            UF <= 0;
+            UB <= 0;
+            int_ena <= 0;
+            int_delay <= 0;
+          end else if (instruction[0:2] == JMS) begin
             if (int_inh == 1'b1) begin
               IF <= IB;
               UF <= UB;
@@ -177,10 +170,10 @@ module mem_ext (
             end
           end
         end
-		EW:;
-		E1:;
-        E2:;
-        E3:;
+        EW: ;
+        E1: ;
+        E2: ;
+        E3: ;
         //H0:;
         H1:
         if (extd_addrd == 1'b1) begin
@@ -195,9 +188,7 @@ module mem_ext (
           IF <= 3'o0;
           IB <= 3'o0;
           DF <= 3'o0;
-          SF <= 6'o0;
           UF <= 0;
-
           int_ena <= 0;
           int_delay <= 0;
           UB <= 0;
--- mem_ext/mem_ext_tb.v
+++ mem_ext/mem_ext_tb.v
@@ -7,116 +7,142 @@
 
 module mem_ext_tb;
 
-    mem_ext ME(.clk (clk),
-        .reset (reset),
-        .instruction (instruction),
-        .sr (sr),
-        .state (state),
-        .clear (clear),
-        .extd_addrd (extd_addrd),
-        .irq (irq),
-        .int_inh (int_inh),
-        .int_in_prog (int_in_prog),
-        .UF (UF),
-        .DF (DF),
-        .IF (IF),
-        .mskip (mskip),
-        .me_bus (me_bus));
+  mem_ext ME (
+      .clk(clk),
+      .reset(reset),
+      .instruction(instruction),
+      .ac (ac),
+      .sr(sr),
+      .state(state),
+      .clear(clear),
+      .extd_addrd(extd_addrd),
+      .irq(irq),
+      .int_inh(int_inh),
+      .int_in_prog(int_in_prog),
+      .UF(UF),
+      .DF(DF),
+      .IF(IF),
+      .UI(UI),
+      .mskip(mskip),
+      .me_bus(me_bus)
+  );
 
-    ma MA (.clk (clk),
-        .reset (reset),
-        .state (state),
-        .instruction (instruction),
-        .int_in_prog (int_in_prog),
-        .eaddr (eaddr),
-        .ac (ac),
-        .sr (sr),
-        .IF (IF),
-        .DF (DF),
-        .addr_loadd (loadd),
-        .depd (depd),
-        .examd (examd),
-        .mdout (mdout));
+  ma MA (
+      .clk(clk),
+      .reset(reset),
+      .state(state),
+      .instruction(instruction),
+      .int_in_prog(int_in_prog),
+      .eaddr(eaddr),
+      .ac(ac),
+      .sr(sr),
+      .IF(IF),
+      .DF(DF),
+      .addr_loadd(loadd),
+      .depd(depd),
+      .examd(examd),
+      .mdout(mdout)
+  );
 
-    state_machine SM1(.clk (clk),
-        .reset (reset),
-        .halt (halt),
-        .cont (cont),
-        .single_step (sing_step),
-        .instruction (instruction),
-        .trigger (trigger),
-        .state (state),
-        .int_ena (int_ena),
-        .int_req (irq),
-        .int_inh (int_inh),
-        .int_in_prog (int_in_prog));
-
-
-    reg clk;
-    reg reset;
-    reg clear;
-    reg extd_addrd;
-    reg halt;
-    reg cont;
-    reg sing_step;
-    reg int_ena;
-    reg irq;
+  state_machine SM1 (
+      .clk(clk),
+      .reset(reset),
+      .halt(halt),
+      .cont(cont),
+      .single_step(sing_step),
+      .instruction(instruction),
+      .trigger(trigger),
+      .state(state),
+      .int_ena(int_ena),
+      .int_req(irq),
+      .int_inh(int_inh),
+      .int_in_prog(int_in_prog)
+  );
 
 
-    reg [0:11] ac;
-    reg [0:11] sr;
+  reg clk;
+  reg reset;
+  reg clear;
+  reg extd_addrd;
+  reg halt;
+  reg cont;
+  reg sing_step;
+  reg int_ena;
+  reg irq;
+  reg loadd;
+  reg trigger;
+  reg examd;
+  reg depd;
+  reg gtf;
 
-    wire [0:14] eaddr;
-    wire [0:11] mdout;
-    wire [0:11] instruction;
-    wire [4:0] state;
-    wire [0:2] DF;
-    wire [0:2] IF;
-    wire UF;
-    wire [0:11] me_bus;
-    wire int_inh;
-    wire int_in_prog;
-    wire skip,mskip;
+
+  reg [0:11] ac;
+  reg [0:11] sr;
+
+  wire [0:14] eaddr;
+  wire [0:11] mdout;
+  wire [0:11] instruction;
+  wire [4:0] state;
+  wire [0:2] DF;
+  wire [0:2] IF;
+  wire UF;
+  wire UI;
+  wire [0:11] me_bus;
+  wire int_inh;
+  wire int_in_prog;
+  wire skip, mskip;
 
 
 
-localparam  clock_period = 1e9/clock_frequency;
-`include "../parameters.v"
-    initial begin
+  localparam clock_period = 1e9 / clock_frequency;
+  `include "../parameters.v"
+  initial begin
 
-        clk <=0;
-        forever
-        begin
-            #(clock_period/2) clk <= 1;
-            #(clock_period/2) clk <= 0;
-        end
+    clk <= 0;
+    forever begin
+      #(clock_period / 2) clk <= 1;
+      #(clock_period / 2) clk <= 0;
     end
+  end
 
-    always @(posedge clk)
-    begin
-        if ((state == E1 ) && (int_in_prog == 1'b1))
-            int_ena <= 0;
+  always @(posedge clk) begin
+    if ((state == E1) && (int_in_prog == 1'b1)) int_ena <= 0;
 
-    end
+  end
 
-    initial begin
-        $dumpfile("mem_ext.vcd");
-        $readmemh("mem_ext.hex", MA.ram.mem,0,8191);
-        $dumpvars(0,ME,MA);
-        halt <= 0;
-        sing_step <= 0;
-        irq <=0;
-        int_ena <= 0;
-        `pulse(reset);
-        `pulse(clear);
-        sr <= 12'o0000;
-		ac <= 12'o0;
-        `pulse(extd_addrd);
-        `pulse(cont);
-        #3800 int_ena <= 1;
-        #236 irq <= 1;
-        #100 $finish;
-    end
+  initial begin
+    $dumpfile("mem_ext.vcd");
+    $readmemh("mem_ext.hex", MA.ram.mem, 0, 8191);
+    $dumpvars(0);
+    #1 halt <= 0;
+    #1 sing_step <= 0;
+    #1 irq <= 0;
+    #1 int_ena <= 0;
+    #1 depd <= 0;
+    #1 examd <= 0;
+    #1 trigger <= 0;
+    #1 extd_addrd <= 0;
+    #1 clear <= 0;
+    #1 gtf <= 0;
+    #1 cont <= 0;
+    #1 loadd <= 0;
+    #100 `pulse(reset);
+    #100 `pulse(clear);
+    sr <= 12'o0200;
+    ac <= 12'o0;
+    // to get the state machine off its base state it needs trigger
+    // at the same time the other signal happens
+    #100 trigger <= 1;
+    #1 loadd <= 1;
+    #140 trigger <= 1;
+    #5 loadd <= 0;
+
+
+    #500 `pulse(cont);
+    #3800 int_ena <= 1;
+    #236 irq <= 1;
+    #100 $finish;
+  end
 
 
 endmodule
