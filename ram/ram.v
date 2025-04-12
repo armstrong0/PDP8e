c3ba06eda12d7b51bad621d544f403cf8ad9ab7a
armstrong0 <d.b.armstrong@sasktel.net>
2025-04-11 20:29:15 -0600

    now has 10k or ram, see ram.v for comments

--- ram/ram.v
+++ ram/ram.v
@@ -1,52 +1,30 @@
-module ram (din, addr, write_en, clk, dout);// 8196 x 12
-    localparam addr_width = 13;
+module ram (din, addr, write_en, clk, dout);// 8196 x 12 + 2048 x 12
+// this is NOT portable
     localparam data_width = 12;
-   // input [addr_width-1:0] addr;
     input [14:0] addr;
     input [data_width-1:0] din;
     input write_en, clk;
     output reg [data_width-1:0] dout;
-    reg [data_width-1:0] mem [(1<<addr_width)-1:0];
+    // the following line defines 10k words, there is probably a formula that
+    // could be used but this seems cleaerer.  I would have prefered to have
+    // the 2048 segement on field 7 in the high half, this puts it in field 2
+    // in the lower half.  After several trys at making it do that I gave up,
+    // it would make the 8 K in block ram and then the rest in logic! 
+    // which would not fit..
+
+    reg [data_width-1:0] mem [15'o23777:0];
+    
 `ifndef SIM
         initial begin
-           //$readmemh("loader.hex",mem,0,4095);// both bin and rim loaders
-           //$readmemh("utils.hex",mem,0,4095);// both bin and rim loaders
-           //$readmemh("focal.hex",mem,0,4095);
            $readmemh("focal_loader.hex",mem,0,4095);
-           //$readmemh("tit_interrupt.hex",mem,0,4095);
-           //$readmemh("../integrate3/Diagnostics/D0AB.hex", mem,0,4095);
-           //$readmemh("../integrate3/Diagnostics/dhkaf-a.hex", mem,0,4095);
-           //$readmemh("../integrate3/Diagnostics/D0BB.hex",mem,0,4095);
-           //$readmemh("../integrate3/Diagnostics/dhkag-a.hex",mem,0,4095);
-           //$readmemh("../integrate3/Diagnostics/D0CC.hex",mem,0,4095);
-           //$readmemh("../integrate3/Diagnostics/D0IB.hex",mem,0,4095);
-           //$readmemh("../integrate3/Diagnostics/D0EB.hex",mem,0,4095);
-           //$readmemh("../integrate3/Diagnostics/D0DB.hex",mem,0,4095);
-           //$readmemh("../integrate3/Diagnostics/D0FC.hex",mem,0,4095);
-           //$readmemh("../integrate3/Diagnostics/D0GC.hex",mem,0,4095);
-           //$readmemh("../integrate3/Diagnostics/D0HC.hex",mem,0,4095);
-           //$readmemh("../integrate3/Diagnostics/D0JB.hex",mem,0,4095);
-           //$readmemh("../integrate3/Diagnostics/dhmca.hex",mem,0,8191);
-           //$readmemh("../integrate3/Diagnostics/d2ab.hex",mem,0,4095);
-           //$readmemh("../EAE/d0la.hex",mem,0,8191);
-
-           //$readmemh("toggle-in-tests.hex",mem,0,4095);
         end
 `endif
-`include "../parameters.v"
-// set up for 8 k, changing requires changing MAX_FILE and the two
-// mem[add[   ]] lines below
-    always @(posedge clk)
-    begin
-    if ((write_en) & (addr[14:12] <= MAX_FIELD))
-        mem[addr[12:0]] <= din;
-    end
 
-    always @(posedge clk)
-    begin
-        if (addr[14:12] > MAX_FIELD)
-            dout <= 12'o0000;
-        else
-            dout <= mem[addr[12:0]]; // Output register controlled by clock.
+	always @(posedge clk)
+    if (write_en == 1)    begin
+	    if (addr < 15'o24000) mem[addr[13:0]] <= din;
     end
+    else
+    if (addr < 15'o24000) dout <= mem[addr[13:0]];
+    else dout <= 12'o0;
 endmodule
